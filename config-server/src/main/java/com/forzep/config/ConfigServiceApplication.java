272bb9bca14df5fa93012a03911aa6f2ffcdf688
qianjing <qianjing@chexiang.com>
2017-11-16 09:53:01 +0800

    第七篇: 高可用的分布式配置中心(Spring Cloud Config) 提交

--- config-server/src/main/java/com/forzep/config/ConfigServiceApplication.java
+++ config-server/src/main/java/com/forzep/config/ConfigServiceApplication.java
@@ -2,10 +2,13 @@ package com.forzep.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+//将服务注册到服务中心
+@EnableDiscoveryClient
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
